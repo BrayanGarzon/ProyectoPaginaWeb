e920fa804c69dd02e5c0d3afee724279fb179452
BrayanGarzon <destroyergarzon@gmail.com>
2020-10-10 20:50:33 -0500

    Nueva pestaña de compra de porducto, pagina web finalizada

--- proyecto Pagina Web/app.js
+++ proyecto Pagina Web/app.js
@@ -1,16 +1,49 @@
+const readline = require('readline');
+
+let datos='';
+
+    let interfazCaptura = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    interfazCaptura.question('\nIngrese el numero que desea: ', function(respuesta){
+        for (let i=1; i<=10; i++){
+        console.log(`${respuesta}*${i}=${respuesta*i}`);
+        datos += `${respuesta}*${i}=${respuesta*i}\n`;
+    }
+/*
+    //impletmentar fs.writefile
+    fs.writeFile('archivo.txt',datos, (err)=>{
+        if(err) throw err;
+        
+        console.log('\nEl archivo se genero bien');
+    });
+*/
+    interfazCaptura.close();
+});
 
 
-//importar
-let base= 2;
-const {crearArchivo} = require ('./multiplicar/multiplicar')
-
-//invocar la funcion de la promesa e implementar
-
-crearArchivo(base).then(archivo => console.log(`archivo creado ${archivo}`))
-.catch(err => console.log(err));
 
 
 
 
+/*const fs = require('fs');
 
+base='2';
+let datos = '';
+
+
+for (let i=1; i<=10; i++){
+    console.log(`${base}*${i}=${base*i}`);
+    datos += `${base}*${i}=${base*i}\n`;
+}
+
+
+//impletmentar fs.writefile
+fs.writeFile('archivo.txt',datos, (err)=>{
+    if(err) throw err;
+    
+    console.log('\nEl archivo se genero bien');
+});*/
 
